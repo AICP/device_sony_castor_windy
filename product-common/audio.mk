32936850bf1ca1bd06f106a8f75dc1a3c77f193f
Max Weffers <rcstar6696@gmail.com>
2017-11-17 13:36:57 +0100

    castor-common: move audio and thermal configs to vendor
    Change-Id: Ice65c26c0b9efab913b6d5d35be902a83cbd98d7

--- product-common/audio.mk
+++ product-common/audio.mk
@@ -1,5 +1,5 @@
 # Audio
 PRODUCT_COPY_FILES += \
-   $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
-   $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
-   $(LOCAL_PATH)/audio/mixer_paths_auxpcm.xml:system/etc/mixer_paths_auxpcm.xml
+   $(LOCAL_PATH)/audio/audio_policy.conf:system/vendor/etc/audio_policy.conf \
+   $(LOCAL_PATH)/audio/mixer_paths.xml:system/vendor/etc/mixer_paths.xml \
+   $(LOCAL_PATH)/audio/mixer_paths_auxpcm.xml:system/vendor/etc/mixer_paths_auxpcm.xml

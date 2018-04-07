68d75181a3f64b66a3246bd6474f76971d7eec8c
SpiritCroc <spiritcroc@gmail.com>
2018-04-07 11:10:39 +0200

    castor_windy: fix build fingerprint mismatch
    Change-Id: Ia311a5a9c7f87a7e7d8bf0a33fcb860a883e475c

--- aicp.mk
+++ aicp.mk
@@ -4,8 +4,9 @@ $(call inherit-product, vendor/aicp/config/common_full_tablet_wifionly.mk)
 $(call inherit-product, device/sony/castor_windy/device.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGP511
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/SGP511/SGP512:6.0.1/23.5.A.1.291/4107128625:user/release-keys
 PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="SGP511-user 6.0.1 23.5.A.1.291 4107128625 release-keys"
+
+BUILD_FINGERPRINT := Sony/SGP511/SGP512:6.0.1/23.5.A.1.291/4107128625:user/release-keys
 
 PRODUCT_NAME := aicp_castor_windy
 PRODUCT_DEVICE := castor_windy
